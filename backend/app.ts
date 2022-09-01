02e89cf8ba92268c3feb03a9a374b3dba8043309
HY Chan <hychan@HYdeMacBook-Air.local>
2022-09-01 15:24:58 +0800

    siusiu backend

---
+++ backend/app.ts
@@ -0,0 +1,97 @@
+// -------------------------------------------------------------------------------------------------------------------
+// imports (DO NOT EXPORT ANYTHING FORM App.ts)
+// -------------------------------------------------------------------------------------------------------------------
+import express from 'express'
+import { logger } from './logger'
+import { isLogin } from './middleware'
+import grant from 'grant'
+import { client } from './db'
+import dotenv from 'dotenv'
+import { UserService } from './services/userService'
+import { UserController } from './controllers/userController'
+import { createUserRoutes } from './routes/userRoutes'
+import Knex from "knex"
+import { createProfileRoutes } from './routes/profileRoutes'
+import { ProfileController } from './controllers/profileController'
+import { ProfileService } from './services/profileService'
+
+
+// -------------------------------------------------------------------------------------------------------------------
+// Knex
+// -------------------------------------------------------------------------------------------------------------------
+
+dotenv.config()
+const knexConfigs = require("./knexfile");
+const configMode = process.env.NODE_ENV || "development";
+const knexConfig = knexConfigs[configMode];
+const knex = Knex(knexConfig);
+
+// -------------------------------------------------------------------------------------------------------------------
+// main script
+// -------------------------------------------------------------------------------------------------------------------
+const app = express()
+const PORT = process.env.PORT || 8000
+
+//grant
+const grantExpress = grant.express({
+	defaults: {
+		origin: 'http://localhost:8000',
+		transport: 'session',
+		state: true
+	},
+	google: {
+		key: process.env.GOOGLE_CLIENT_ID || '',
+		secret: process.env.GOOGLE_CLIENT_SECRET || '',
+		scope: ['profile', 'email'],
+		callback: '/login/google'
+	}
+})
+
+
+app.use(grantExpress as express.RequestHandler)
+// -------------------------------------------------------------------------------------------------------------------
+// others
+// -------------------------------------------------------------------------------------------------------------------
+
+//connect to client
+client.connect()
+
+//urlencoded
+app.use(express.urlencoded({ extended: true }))
+
+//json
+app.use(express.json())
+
+//get HTML files from public, default images & uploads
+app.use(express.static('public'))//get files from private
+const userService = new UserService(knex)
+const profileService = new ProfileService(knex)
+
+const userController = new UserController(userService)
+app.use(isLogin, express.static('private'))	
+
+app.use('/serverDefaultedImages', express.static('images'))
+app.use('/userUploadedFiles', express.static('uploads'))
+
+// get code from usersRoute
+const profileController = new ProfileController(profileService)
+
+app.use(createUserRoutes(userController))
+app.use(createProfileRoutes(profileController))
+
+// --------------------------------------------------------------------------------------------------------------------
+// Error 404
+//---------------------------------------------------------------------------------------------------------------------
+
+app.use((req, res) =>{
+	res.status(404)
+	res.send('404 Not Found')
+})
+
+// --------------------------------------------------------------------------------------------------------------------
+// listening
+//---------------------------------------------------------------------------------------------------------------------
+app.listen(PORT, () => {
+	logger.info(`listening on port ${PORT}`)
+})
+
