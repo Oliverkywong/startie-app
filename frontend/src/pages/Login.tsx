04103657250f955257c91521ad89766e5b3cf139
Kai yuen Wong <oliverwong@Kais-MacBook-Air.local>
2022-09-01 15:40:56 +0800

    open frontend folder

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { IonContent, IonPage } from '@ionic/react'
+
+const Login: React.FC = () => {
+  return (
+    <IonPage className="login">
+      <IonContent>Login</IonContent>
+    </IonPage>
+  )
+}
+
+export default Login;
