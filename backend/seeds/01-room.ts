663ff60bbd57ab8e70b3953bb997126875d3bd0a
HY Chan <hychan@HYdeMacBook-Air.local>
2022-09-02 18:50:55 +0800

    seed -ing  backend

---
+++ backend/seeds/01-room.ts
@@ -0,0 +1,36 @@
+import { Knex } from "knex";
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex("room").del();
+
+    // Inserts seed entries
+    await knex
+    
+    .insert([
+        { 
+          
+            created_at: new Date(),
+            updated_at: new Date(),
+
+        },
+        {
+      
+            created_at: new Date(),
+            updated_at: new Date(),
+
+        },
+        {
+          
+            created_at: new Date(),
+            updated_at: new Date(),
+            
+        }
+
+
+
+    ])
+        .into("room")
+
+
+}
