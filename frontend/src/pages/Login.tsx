32977eadfe1c56263caa14628285bac2f5dab727
Kai yuen Wong <oliverwong@Kais-MacBook-Air.local>
2022-09-03 15:14:44 +0800

    complete profile

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,50 +1,46 @@
 import React from 'react'
-import { IonButton, IonContent, IonIcon, IonImg, IonInput, IonPage } from '@ionic/react'
+import { IonButton, IonIcon, IonImg, IonPage } from '@ionic/react'
 import logo from '../img/StartieLogo.png'
-import './Login.css'
+import './css/Login.css'
 import { eyeOffOutline, eyeOutline, lockClosedOutline, mailOutline } from 'ionicons/icons'
 
 
 const Login: React.FC = () => {
 
   const [email, setEmail] = React.useState('')
-
   const [password, setPassword] = React.useState({
     password: "",
     showPassword: false,
   });
-
-  const handleClickShowPassword = () => {
-    setPassword({ ...password, showPassword: !password.showPassword });
-  };
-
-  const handlePasswordChange = (prop: string) => (e: {target: { value: string }}) => {
-    setPassword({ ...password, [prop]: e.target.value });
-  };
 
   return (
     <IonPage>
       <div className='background' >
         <IonImg src={logo} className="logo" />
         <h1>Hey, Welcome Back!</h1>
+        <form onSubmit={(e) => e.preventDefault()}>
         <div className='email'>
           <IonIcon icon={mailOutline} />
           <input type="email" placeholder="Email"
             value={email}
-            onChange={(e)=>setEmail(e.target.value)} />
+            onChange={(e) => setEmail(e.target.value)} />
         </div>
         <div className='password'>
           <IonIcon icon={lockClosedOutline} />
           <input
             type={password.showPassword ? "text" : "password"}
-            onChange={handlePasswordChange("password")}
+            onChange={(e) => setPassword({ ...password, password: e.target.value })}
             placeholder="Password"
             value={password.password}
           />
-          <IonIcon icon={password.showPassword ? eyeOutline : eyeOffOutline} onClick={handleClickShowPassword} /><br />
-          <IonButton color="danger">Continue</IonButton>
-          <a href='#'>Forgot Password?</a>
+          <IonIcon
+            icon={password.showPassword ? eyeOutline : eyeOffOutline}
+            onClick={() => setPassword({ ...password, showPassword: !password.showPassword })}
+          /><br />
         </div>
+          <button type="submit">Continue</button>
+          </form>
+          <a href='#'>Forgot Password?</a>
         <div className='signup'>
           <p>New to Startie?<a href='#'>Sign Up</a></p>
         </div>
---
+++ frontend/src/pages/UserFollows.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function UserFollows() {
+  return (
+    <div>UserFollows</div>
+  )
+}
