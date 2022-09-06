3f515528985ad1585c5a24838c783a174c94f5c0
SevenBoii <assy2166@gmail.com>
2022-09-06 16:17:32 +0800

    teamService, jobService and eventService test case update

--- backend/services/unit-test/eventService.test.ts
+++ backend/services/unit-test/eventService.test.ts
@@ -5,8 +5,20 @@ dotenv.config();
 const knexfile = require("../../knexfile");
 const knex = Knex(knexfile["test"]);
 
-describe("EventService", () => {
+describe("EventService CRUD", () => {
   let eventService = new EventService(knex);
+
+  const eventInfo = {
+    name: "eventInfoName",
+    description: "eventInfoDescription",
+    profilepic: "eventInfoProfilepic",
+  };
+
+  const newEventInfo = {
+    name: "newEventInfoName",
+    description: "newEventInfoDescription",
+    profilepic: "newEventInfoProfilepic",
+  };
 
   beforeAll(async () => {
     return knex.migrate
@@ -23,17 +35,19 @@ describe("EventService", () => {
     await knex.destroy();
   });
 
+  beforeEach(async () => {});
+
   it("function createEvent test", async () => {
     const time = new Date();
     const createEvent = await eventService.createEvent(
-      "eventtest",
-      "eventtest",
-      "eventtest",
+      eventInfo.name,
+      eventInfo.description,
+      eventInfo.profilepic,
       time
     );
-    expect(createEvent[0].name).toBe("eventtest");
-    expect(createEvent[0].description).toBe("eventtest");
-    expect(createEvent[0].profilepic).toBe("eventtest");
+    expect(createEvent[0].name).toBe(eventInfo.name);
+    expect(createEvent[0].description).toBe(eventInfo.description);
+    expect(createEvent[0].profilepic).toBe(eventInfo.profilepic);
     expect(createEvent[0].starttime).toStrictEqual(time);
   });
 
@@ -42,23 +56,21 @@ describe("EventService", () => {
     expect(getAllEvents.length).toBeGreaterThan(0);
   });
 
-  it("function getEvent test", async () => {
-    const getEvent = await eventService.getEvent("eventtest");
-    expect(getEvent[0].name).toBe("eventtest");
-  });
+  it("function getEvent and updateEvent test", async () => {
+    const getEvent = await eventService.getEvent(eventInfo.name);
+    expect(getEvent[0].name).toBe(eventInfo.name);
 
-  it("function updateEvent test", async () => {
     const time = new Date();
     const updateEvent = await eventService.updateEvent(
-      4,
-      "eventtest2",
-      "eventtest2",
-      "eventtest2",
+      getEvent[0].id,
+      newEventInfo.name,
+      newEventInfo.description,
+      newEventInfo.profilepic,
       time
     );
-    expect(updateEvent![0].name).toBe("eventtest2");
-    expect(updateEvent![0].description).toBe("eventtest2");
-    expect(updateEvent![0].profilepic).toBe("eventtest2");
+    expect(updateEvent![0].name).toBe(newEventInfo.name);
+    expect(updateEvent![0].description).toBe(newEventInfo.description);
+    expect(updateEvent![0].profilepic).toBe(newEventInfo.profilepic);
     expect(updateEvent![0].starttime).toStrictEqual(time);
   });
 });
--- backend/services/unit-test/jobService.test.ts
+++ backend/services/unit-test/jobService.test.ts
@@ -6,8 +6,18 @@ dotenv.config();
 const knexfile = require("../../knexfile");
 const knex = Knex(knexfile["test"]);
 
-describe("JobService", () => {
+describe("JobService CRUD", () => {
   let jobService = new JobService(knex);
+
+  const jobInfo = {
+    name: "jobName",
+    description: "jobDescription",
+  };
+
+  const newJobInfo = {
+    name: "newJobName",
+    description: "newJobDescription",
+  };
 
   beforeAll(async () => {
     return knex.migrate
@@ -25,9 +35,12 @@ describe("JobService", () => {
   });
 
   it("function createJob test", async () => {
-    const createJob = await jobService.createJob("jobtest", "jobtest");
-    expect(createJob[0].name).toBe("jobtest");
-    expect(createJob[0].description).toBe("jobtest");
+    const createJob = await jobService.createJob(
+      jobInfo.name,
+      jobInfo.description
+    );
+    expect(createJob[0].name).toBe(jobInfo.name);
+    expect(createJob[0].description).toBe(jobInfo.description);
   });
 
   it("function getAllJobs test", async () => {
@@ -35,20 +48,24 @@ describe("JobService", () => {
     expect(getAllJobs.length).toBeGreaterThan(0);
   });
 
-  it("function getJob test", async () => {
-    const getJob = await jobService.getJob("jobtest");
-    expect(getJob[0].name).toBe("jobtest");
-  });
+  it("function getJob, updateJob and deleteJob test", async () => {
+    const getJob = await jobService.getJob(jobInfo.name);
+    expect(getJob.length).toBeGreaterThan(0);
+    expect(getJob[0].name).toBe(jobInfo.name);
 
-  it("function updateJob test", async () => {
-    const updateJob = await jobService.updateJob(4, "jobtest2", "jobtest2");
-    expect(updateJob![0].name).toBe("jobtest2");
-    expect(updateJob![0].description).toBe("jobtest2");
-  });
+    const updateJob = await jobService.updateJob(
+      getJob[0].id,
+      newJobInfo.name,
+      newJobInfo.description
+    );
+    expect(updateJob![0].name).toBe(newJobInfo.name);
+    expect(updateJob![0].description).toBe(newJobInfo.description);
 
-  it("function deleteJob test", async () => {
-    await jobService.deleteJob(4);
-    const result = knex<Job>("job").select("*").where("id", 4).returning("*");
-    expect((await result).length).toBe(0);
+    await jobService.deleteJob(getJob[0].id);
+    const result = await knex<Job>("job")
+      .select("*")
+      .where("id", getJob[0].id)
+      .returning("*");
+    expect(result.length).toBe(0);
   });
 });
