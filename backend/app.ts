76432b1ed379816458ef58a044ab25e8735a19ea
Oliverkywong <tom23400@gmail.com>
2022-09-08 18:29:30 +0800

    fix typo mistake

--- backend/app.ts
+++ backend/app.ts
@@ -12,8 +12,8 @@ import { userRoutes } from './routes/userRoute'
 import Knex from "knex"
 import cors from 'cors'
 import { sectorskillRoutes } from "./routes/sectorskillRoute";
-import { SectorskillService } from './services/SectorSkillService'
-import { SectorskillController } from './controllers/SectorSkillController'
+import { SectorskillService } from './services/sectorSkillService'
+import { SectorskillController } from './controllers/sectorSkillController'
 // import { isLogin } from './utils/middleware'
 
 
