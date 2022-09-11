2b845a38f6fb1a352804430753e1629abe8f4947
SevenBoii <assy2166@gmail.com>
2022-09-11 23:12:51 +0800

    merge

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,73 +1,82 @@
-import React, { useState } from 'react'
-import { IonIcon, IonImg, IonPage,useIonRouter } from '@ionic/react'
-import logo from '../img/StartieLogo.png'
-import { eyeOffOutline, eyeOutline, lockClosedOutline, personOutline } from 'ionicons/icons'
+import React, { useState } from "react";
+import { IonIcon, IonImg, IonPage, useIonRouter } from "@ionic/react";
+import logo from "../img/StartieLogo.png";
+import {
+  eyeOffOutline,
+  eyeOutline,
+  lockClosedOutline,
+  personOutline,
+} from "ionicons/icons";
 import { useForm } from "react-hook-form";
-import './css/Login.css'
-import { useDispatch } from 'react-redux';
-import { loggedIn } from '../redux/auth/action';
-
+import { useDispatch } from "react-redux";
+import { loggedIn } from "../redux/auth/action";
+import { render } from "react-dom";
 
 const Login: React.FC = () => {
-
   const { register, handleSubmit } = useForm();
   const [passwordShown, setPasswordShown] = useState(false);
   const dispatch = useDispatch();
   const router = useIonRouter();
 
-      
   return (
     <IonPage>
-      <div className='background' >
+      <div className="background">
         <IonImg src={logo} className="logo" />
         <h1>Hey, Welcome Back!</h1>
-        <form onSubmit={
-          handleSubmit(async data => {
-            const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/login`, {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(data)
-            })
+        <form
+          onSubmit={handleSubmit(async (data) => {
+            const res = await fetch(
+              `${process.env.REACT_APP_BACKEND_URL}/login`,
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data),
+              }
+            );
 
-            if(res.status === 200) {
-              const userRecord = await res.json()
+            if (res.status === 200) {
+              const userRecord = await res.json();
               // console.log(userRecord)
-              dispatch(loggedIn(userRecord['user'], userRecord['token']))
-              localStorage.setItem('token', userRecord['token'])
+              dispatch(loggedIn(userRecord["user"], userRecord["token"]));
+              localStorage.setItem("token", userRecord["token"]);
               router.push("/tab/home");
             }
-          })}>
-          <div className='username'>
+          })}
+        >
+          <div className="username">
             <IonIcon icon={personOutline} />
             <input
-              {...register('username')}
+              {...register("username")}
               type="text"
-              placeholder='User Name'
+              placeholder="User Name"
             />
           </div>
-          <div className='password'>
+          <div className="password">
             <IonIcon icon={lockClosedOutline} />
             <input
-              {...register('password')}
-              placeholder='Password'
+              {...register("password")}
+              placeholder="Password"
               type={passwordShown ? "text" : "password"}
             />
             <IonIcon
               icon={passwordShown ? eyeOutline : eyeOffOutline}
               onClick={() => setPasswordShown(passwordShown ? false : true)}
-            /><br />
+            />
+            <br />
           </div>
           <button type="submit">Continue</button>
         </form>
-        <a href='#'>Forgot Password?</a>
-        <div className='signup'>
-          <p>New to Startie?<a href='/signup'>Sign Up</a></p>
+        <a href="#">Forgot Password?</a>
+        <div className="signup">
+          <p>
+            New to Startie?<a href="/signup">Sign Up</a>
+          </p>
         </div>
       </div>
-    </IonPage >
-  )
-}
+    </IonPage>
+  );
+};
 
 export default Login;
