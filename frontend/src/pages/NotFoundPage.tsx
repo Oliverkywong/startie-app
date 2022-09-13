96eb00f726d7d5ece97ce1a99af1ed32f57435ad
SevenBoii <assy2166@gmail.com>
2022-09-13 18:10:04 +0800

    css update

--- frontend/src/pages/NotFoundPage.tsx
+++ frontend/src/pages/NotFoundPage.tsx
@@ -1,33 +1,38 @@
 import {
-    IonButton,
-    IonContent,
-    IonHeader,
-    IonPage,
-    IonTitle,
-    IonToolbar
-  } from '@ionic/react'
-  
-  const NotFoundPage: React.FC = () => {
-    let title = 'Page Not Found'
-    return (
-      <IonPage>
-        <IonHeader>
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonLabel,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonImg,
+} from "@ionic/react";
+
+const NotFoundPage: React.FC = () => {
+  let title = "We're sorry";
+  let content =
+    "The page you requested could not be found. Please go back to the homepage.";
+  let img = "../img/notFoundPage.png";
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonImg src={img} />
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className="ion-padding">
+        <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle>{title}</IonTitle>
+            <IonTitle size="large">{title}</IonTitle>
+            <IonLabel>{content}</IonLabel>
           </IonToolbar>
         </IonHeader>
-        <IonContent fullscreen className="ion-padding">
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">{title}</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonButton routerLink={'/login'}>Back to Home</IonButton>
+        <IonButton routerLink={"/login"}>Return to Homepage</IonButton>
+      </IonContent>
+    </IonPage>
+  );
+};
 
-        </IonContent>
-      </IonPage>
-    )
-  }
-  
-  export default NotFoundPage
-  
+export default NotFoundPage;
