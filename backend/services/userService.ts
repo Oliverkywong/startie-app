14e10011a79de7fb57200b3e21f45eb6e2e272c1
SevenBoii <assy2166@gmail.com>
2022-09-15 17:00:22 +0800

    profile can show user's team now

--- backend/services/userService.ts
+++ backend/services/userService.ts
@@ -1,251 +1,261 @@
-import { Knex } from 'knex'
-import { checkPassword, hashPassword } from '../utils/hash'
-import { User } from '../utils/model'
+import { Knex } from "knex";
+import { checkPassword, hashPassword } from "../utils/hash";
+import { User } from "../utils/model";
 
 export class UserDuplicateUsernameError extends Error {
-	constructor(msg?: string) {
-		super(msg)
-		Object.setPrototypeOf(this, UserDuplicateUsernameError.prototype)
-	}
+  constructor(msg?: string) {
+    super(msg);
+    Object.setPrototypeOf(this, UserDuplicateUsernameError.prototype);
+  }
 }
 
 export class UserDuplicateEmailError extends Error {
-	constructor(msg?: string) {
-		super(msg)
-		Object.setPrototypeOf(this, UserDuplicateEmailError.prototype)
-	}
+  constructor(msg?: string) {
+    super(msg);
+    Object.setPrototypeOf(this, UserDuplicateEmailError.prototype);
+  }
 }
 
 export class UserMissingRegisterInfoError extends Error {
-	constructor(msg?: string) {
-		super(msg)
-		Object.setPrototypeOf(this, UserMissingRegisterInfoError.prototype)
-	}
+  constructor(msg?: string) {
+    super(msg);
+    Object.setPrototypeOf(this, UserMissingRegisterInfoError.prototype);
+  }
 }
 
 export class UserNotExistError extends Error {
-	constructor(msg?: string) {
-		super(msg)
-		Object.setPrototypeOf(this, UserNotExistError.prototype)
-	}
+  constructor(msg?: string) {
+    super(msg);
+    Object.setPrototypeOf(this, UserNotExistError.prototype);
+  }
 }
 
 export class UserPasswordMissMatchError extends Error {
-	constructor(msg?: string) {
-		super(msg) //super is used to call the parent class constructor
-		Object.setPrototypeOf(this, UserPasswordMissMatchError.prototype)
-	}
+  constructor(msg?: string) {
+    super(msg); //super is used to call the parent class constructor
+    Object.setPrototypeOf(this, UserPasswordMissMatchError.prototype);
+  }
 }
 
 export class UserStatusError extends Error {
-	constructor(msg?: string) {
-		super(msg) //super is used to call the parent class constructor
-		Object.setPrototypeOf(this, UserStatusError.prototype)
-	}
+  constructor(msg?: string) {
+    super(msg); //super is used to call the parent class constructor
+    Object.setPrototypeOf(this, UserStatusError.prototype);
+  }
 }
 
 export class UserService {
-	constructor(private knex: Knex) { }
+  constructor(private knex: Knex) {}
 
-	// -------------------------------------------------------------------------------------------------------------------
-	// Register ✅
-	// -------------------------------------------------------------------------------------------------------------------
+  // -------------------------------------------------------------------------------------------------------------------
+  // Register ✅
+  // -------------------------------------------------------------------------------------------------------------------
 
-	async register(
-		username: string,
-		password: string,
-		email: string,
-		statusId: number
-	) {
-		{
-			const userEmailRecord = await this.knex<User>('user')
-				.select('*')
-				.where('email', email)
+  async register(
+    username: string,
+    password: string,
+    email: string,
+    statusId: number
+  ) {
+    {
+      const userEmailRecord = await this.knex<User>("user")
+        .select("*")
+        .where("email", email);
 
-			const userRecord = await this.knex<User>('user')
-				.select('*')
-				.where('username', username)
+      const userRecord = await this.knex<User>("user")
+        .select("*")
+        .where("username", username);
 
-			if (userRecord.length > 0) {
-				throw new UserDuplicateUsernameError()
-			}
+      if (userRecord.length > 0) {
+        throw new UserDuplicateUsernameError();
+      }
 
-			if (userEmailRecord.length > 0) {
-				throw new UserDuplicateEmailError()
-			}
+      if (userEmailRecord.length > 0) {
+        throw new UserDuplicateEmailError();
+      }
 
-			if (!username || !password || !email) {
-				throw new UserMissingRegisterInfoError()
-			}
-		}
+      if (!username || !password || !email) {
+        throw new UserMissingRegisterInfoError();
+      }
+    }
 
-		// insert user
-		await this.knex<User>('user').insert({
-			username: username,
-			password: await hashPassword(password),
-			email: email,
-			status_id: statusId
-		})
+    // insert user
+    await this.knex<User>("user").insert({
+      username: username,
+      password: await hashPassword(password),
+      email: email,
+      status_id: statusId,
+    });
 
-		return true
-	}
+    return true;
+  }
 
-	// -------------------------------------------------------------------------------------------------------------------
-	// Login Google
-	// -------------------------------------------------------------------------------------------------------------------
-	// async loginGoogle(accessToken: string) {
-	// 	const fetchRes = await fetch(
-	// 		'https://www.googleapis.com/oauth2/v2/userinfo',
-	// 		{
-	// 			method: 'get',
-	// 			headers: {
-	// 				Authorization: `Bearer ${accessToken}`
-	// 			}
-	// 		}
-	// 	)
+  // -------------------------------------------------------------------------------------------------------------------
+  // Login Google
+  // -------------------------------------------------------------------------------------------------------------------
+  // async loginGoogle(accessToken: string) {
+  // 	const fetchRes = await fetch(
+  // 		'https://www.googleapis.com/oauth2/v2/userinfo',
+  // 		{
+  // 			method: 'get',
+  // 			headers: {
+  // 				Authorization: `Bearer ${accessToken}`
+  // 			}
+  // 		}
+  // 	)
 
-	// 	const result = await fetchRes.json()
+  // 	const result = await fetchRes.json()
 
-	// 	// const users = (
-	// 	// 	await this.knex.query(
-	// 	// 		`SELECT * FROM users WHERE users.username = $1`,
-	// 	// 		[result.email]
-	// 	// 	)
-	// 	// ).rows
+  // 	// const users = (
+  // 	// 	await this.knex.query(
+  // 	// 		`SELECT * FROM users WHERE users.username = $1`,
+  // 	// 		[result.email]
+  // 	// 	)
+  // 	// ).rows
 
-	// 	let users = this.knex<User>('user')
-	// 		.select('*')
-	// 		.where('username', result.email)
+  // 	let users = this.knex<User>('user')
+  // 		.select('*')
+  // 		.where('username', result.email)
 
-	// 	users = await // `SELECT * FROM users WHERE users.username = $1`,
-	// 	// [result.email]
+  // 	users = await // `SELECT * FROM users WHERE users.username = $1`,
+  // 	// [result.email]
 
-	// 	this.knex<User>('user').select('*').where('username', result.email)[0]
-	// 		.rows
+  // 	this.knex<User>('user').select('*').where('username', result.email)[0]
+  // 		.rows
 
-	// 	let user = users[0]
+  // 	let user = users[0]
 
-	// 	let username = result.name || result.email
+  // 	let username = result.name || result.email
 
-	// 	if (!user) {
-	// 		return user
-	// 	} else {
-	// 		const userRecord = await this.knex<User>('user')
+  // 	if (!user) {
+  // 		return user
+  // 	} else {
+  // 		const userRecord = await this.knex<User>('user')
 
-	// 			.insert({
-	// 				username,
-	// 				password: await hashPassword(result.email),
-	// 				email: result.email,
-	// 				role_id: 1,
-	// 				status_id: 1
-	// 			})
-	// 			.returning('*')
-	// 		// `INSERT INTO users (username,password,created_at,updated_at) VALUES ($1,$2,NOW(),NOW()) RETURNING *`,
-	// 		// 			[username, await hashPassword(username)]
-	// 		return userRecord
-	// 	}
-	// }
-	// -------------------------------------------------------------------------------------------------------------------
-	// Login ✅
-	// -------------------------------------------------------------------------------------------------------------------
+  // 			.insert({
+  // 				username,
+  // 				password: await hashPassword(result.email),
+  // 				email: result.email,
+  // 				role_id: 1,
+  // 				status_id: 1
+  // 			})
+  // 			.returning('*')
+  // 		// `INSERT INTO users (username,password,created_at,updated_at) VALUES ($1,$2,NOW(),NOW()) RETURNING *`,
+  // 		// 			[username, await hashPassword(username)]
+  // 		return userRecord
+  // 	}
+  // }
+  // -------------------------------------------------------------------------------------------------------------------
+  // Login ✅
+  // -------------------------------------------------------------------------------------------------------------------
 
-	async login(username: string, password: string) {
+  async login(username: string, password: string) {
+    let result = this.knex<User>("user")
+      .select("*")
+      .where("username", username);
 
-		let result = this.knex<User>('user')
-			.select('*')
-			.where('username', username)
+    const userRecord = await result;
 
-		const userRecord = await result
+    if (userRecord.length === 0) {
+      throw new UserNotExistError();
+    }
 
-		if (userRecord.length === 0) {
-			throw new UserNotExistError()
-		}
+    if (
+      !(
+        username === userRecord[0].username &&
+        (await checkPassword(password, userRecord[0].password))
+      )
+    ) {
+      throw new UserPasswordMissMatchError();
+    }
+    if (userRecord[0].status_id != 1) {
+      throw new UserStatusError();
+    }
+    return userRecord;
+  }
+  // -------------------------------------------------------------------------------------------------------------------
+  // get self UserInfo by ID
+  // -------------------------------------------------------------------------------------------------------------------
+  async userInfo(userId: number) {
+    const userRecord = await this.knex<User>("user")
+      .select("*")
+      .where("id", userId);
 
-		if (
-			!(
-				username === userRecord[0].username &&
-				(await checkPassword(password, userRecord[0].password))
-			)
-		) {
-			throw new UserPasswordMissMatchError()
-		}
-		if (userRecord[0].status_id != 1) {
-			throw new UserStatusError()
-		}
-		return userRecord
-	}
-// -------------------------------------------------------------------------------------------------------------------
-// get self UserInfo by ID
-// -------------------------------------------------------------------------------------------------------------------
-	async userInfo(userId: number) {
-		const userRecord = await this.knex<User>('user')
-			.select('*')
-			.where('id', userId)
+    return userRecord;
+  }
+  // -------------------------------------------------------------------------------------------------------------------
+  // get all UserInfo
+  // -------------------------------------------------------------------------------------------------------------------
+  async getAllUser() {
+    const userRecord = await this.knex<User>("user").select("*");
 
-			return userRecord
-	}
-// -------------------------------------------------------------------------------------------------------------------
-// get all UserInfo
-// -------------------------------------------------------------------------------------------------------------------
-	async getAllUser() {
-		const userRecord = await this.knex<User>('user')
-			.select('*')
+    return userRecord;
+  }
+  // -------------------------------------------------------------------------------------------------------------------
+  // edit User Info
+  // -------------------------------------------------------------------------------------------------------------------
+  async editUser(
+    userId: number,
+    newProfilepic: string,
+    // newStatusId?: number,
+    newPhoneNumber: string,
+    newDescription: string
+  ) {
+    const userRecord = await this.knex<User>("user")
+      .update({
+        profilepic: newProfilepic,
+        phonenumber: newPhoneNumber,
+        // status_id: newStatusId,
+        description: newDescription,
+      })
+      .where("id", userId)
+      .returning("*");
 
-		return userRecord
-}
-// -------------------------------------------------------------------------------------------------------------------
-// edit User Info
-// -------------------------------------------------------------------------------------------------------------------
-	async editUser(
-		userId: number,
-		newProfilepic: string,
-		// newStatusId?: number,
-		newPhoneNumber: string,
-		newDescription: string
-	) {
-			const userRecord = await this.knex<User>('user')
-				.update({
-					profilepic: newProfilepic,
-					phonenumber: newPhoneNumber,
-					// status_id: newStatusId,
-					description: newDescription
-				})
-				.where('id', userId)
-				.returning('*')
+    return userRecord;
+  }
 
-			return userRecord
-		} 
-	
-//--------------------------------------------------------------------------------------------------------------------------------
-//get all users
-//--------------------------------------------------------------------------------------------------------------------------------
-	async getAllUsers() {
-		return await this.knex('user').select('*')
-	}
+  // --------------------------------------------------------------------------------------------------------------------------------
+  //get all users
+  // --------------------------------------------------------------------------------------------------------------------------------
+  async getAllUsers() {
+    return await this.knex("user").select("*");
+  }
 
-	// async appleLogin(username: string, email: string) {
-	// 	const userEmailRecord = await this.knex<User>('user')
-	// 		.select('*')
-	// 		.where('email', email)
+  // async appleLogin(username: string, email: string) {
+  // 	const userEmailRecord = await this.knex<User>('user')
+  // 		.select('*')
+  // 		.where('email', email)
 
-	// 	const userRecord = await this.knex<User>('user')
-	// 		.select('*')
-	// 		.where('username', username)
+  // 	const userRecord = await this.knex<User>('user')
+  // 		.select('*')
+  // 		.where('username', username)
 
-	// 	if (userRecord.length > 0) {
-	// 		throw new UserDuplicateUsernameError()
-	// 	}
+  // 	if (userRecord.length > 0) {
+  // 		throw new UserDuplicateUsernameError()
+  // 	}
 
-	// 	if (userEmailRecord.length > 0) {
-	// 		throw new UserDuplicateEmailError()
-	// 	}
-	// 	await this.knex<User>('user').insert({
-	// 		username: username,
-	// 		password: 'apple',
-	// 		email: email,
-	// 		status_id: 1
-	// 	})
+  // 	if (userEmailRecord.length > 0) {
+  // 		throw new UserDuplicateEmailError()
+  // 	}
+  // 	await this.knex<User>('user').insert({
+  // 		username: username,
+  // 		password: 'apple',
+  // 		email: email,
+  // 		status_id: 1
+  // 	})
 
-	// 	return true
-	// }
+  // 	return true
+  // }
+
+  // -------------------------------------------------------------------------------------------------------------------
+  // check team
+  // -------------------------------------------------------------------------------------------------------------------
+  async checkTeam(userId: number) {
+    const userRecord = await this.knex<User>("user_team")
+      .join("team", "user_id", "team.id")
+      .select("*")
+      .where("user_id", userId);
+
+    return userRecord;
+  }
 }
