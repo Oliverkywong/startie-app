61e2a39791db14eeef9babb9a2df8b942e23cf9b
SevenBoii <assy2166@gmail.com>
2022-09-16 17:22:01 +0800

    profile, userTeam css update

--- frontend/src/pages/UserTeams.tsx
+++ frontend/src/pages/UserTeams.tsx
@@ -1,16 +1,26 @@
-import { IonImg, IonItem, IonLabel } from "@ionic/react";
+import { IonImg, IonItem, IonLabel, IonTitle } from "@ionic/react";
 import React from "react";
 import { Team } from "../model";
+
+import "./css/Common.css";
+import "./css/UserTeam.css";
 
 export default function UserTeams(props: { team: Team[] }) {
   return (
     <div style={{ color: "#000" }}>
       {props.team.map((team) => {
         return (
-          <IonItem key={team.id}>
-            <IonLabel>{team.name}</IonLabel>
-            <IonLabel>{team.description}</IonLabel>
-            <IonImg src={team.profilepic}></IonImg>
+          <IonItem className="userTeam" key={team.id}>
+            <div>
+              <IonImg src={team.profilepic}></IonImg>
+            </div>
+            <div>
+              <IonTitle>{team.name}</IonTitle>
+            </div>
+
+            <div>
+              <IonLabel>{team.description}</IonLabel>
+            </div>
           </IonItem>
         );
       })}
