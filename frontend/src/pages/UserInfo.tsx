2a50832cc0e7460b71e12b82cdda43bcd9b008d8
SevenBoii <assy2166@gmail.com>
2022-09-19 17:59:07 +0800

    merge

--- frontend/src/pages/UserInfo.tsx
+++ frontend/src/pages/UserInfo.tsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 export default function UserInfo(props: { description: string | undefined }) {
   return (
     <div style={{ color: "black" }}>
-      UserInfo
       <p>{props.description}</p>
     </div>
   );
--- frontend/src/pages/UserTeams.tsx
+++ frontend/src/pages/UserTeams.tsx
@@ -1,4 +1,11 @@
-import { IonContent, IonImg, IonItem, IonLabel, IonTitle } from "@ionic/react";
+import {
+  IonContent,
+  IonImg,
+  IonItem,
+  IonLabel,
+  IonTitle,
+  useIonRouter,
+} from "@ionic/react";
 import React from "react";
 import { Team } from "../model";
 
@@ -6,12 +13,17 @@ import "./css/Common.css";
 import "./css/UserTeam.css";
 
 export default function UserTeams(props: { team: Team[] }) {
+  const router = useIonRouter();
   return (
     <IonContent>
       <div style={{ color: "#000" }}>
         {props.team.map((team) => {
           return (
-            <div className="userTeam" key={team.id}>
+            <div
+              className="userTeam"
+              key={team.id}
+              onClick={() => router.push(`team/${team.id}`)}
+            >
               <IonImg
                 src={
                   team?.profilepic != null
