fc35cb543a75edbc84365704cd266f17dfad3970
SevenBoii <assy2166@gmail.com>
2022-09-21 14:16:18 +0800

    teamList css update

--- frontend/src/pages/EventDetail.tsx
+++ frontend/src/pages/EventDetail.tsx
@@ -2,23 +2,17 @@ import {
   IonPage,
   IonHeader,
   IonImg,
-  IonButton,
   IonBackButton,
   IonButtons,
   IonToolbar,
   useIonRouter,
-  IonCard,
-  IonCardContent,
-  IonItem,
-  IonLabel,
   IonContent,
-  IonCardTitle,
 } from "@ionic/react";
 import "./css/Event.css";
+import "./css/EventDetail.css";
 import "./css/Common.css";
 import React, { useEffect, useState } from "react";
 import { useLocation, useRouteMatch } from "react-router-dom";
-import eventimg from "../img/com1.png";
 
 interface EventDetail {
   id: number;
@@ -80,7 +74,7 @@ const EventDetail: React.FC = () => {
       <IonContent>
         {data.map((item) => {
           return (
-            <IonCard key={item.id} className="eventDetail">
+            <div key={item.id}>
               <IonImg
                 className="eventThumbnail"
                 src={
@@ -89,30 +83,19 @@ const EventDetail: React.FC = () => {
                     : "StartieLogo.png"
                 }
               />
-              <IonCardTitle className="evenDetailTitle">
-                {item.name}
-              </IonCardTitle>
-              <div className="event">
-                <IonImg
-                  src={
-                    item?.profilepic != null
-                      ? `${process.env.REACT_APP_BACKEND_URL}/userUploadedFiles/${item.profilepic}`
-                      : "StartieLogo.png"
-                  }
-                  style={{ width: "10%" }}
-                />
-                <div className="eventinfo">
-                  <IonLabel className="eventDescription">
-                    {item.description}
-                  </IonLabel>
-                  <IonLabel>{item.starttime}</IonLabel>
-                </div>
+              <h1 className="evenDetailTitle">{item.name}</h1>
+
+              <div>
+                <p className="eventDetailDescription">{item.description}</p>
+                <p className="eventDetailDate">Due date: {item.starttime}</p>
               </div>
-            </IonCard>
+            </div>
           );
         })}
-        <div className="detailButton">
-          <IonButton onClick={joinEvent}>Join Competition</IonButton>
+        <div className="eventButtonContainer">
+          <button className="eventDetailButton" onClick={joinEvent}>
+            Join Competition
+          </button>
         </div>
       </IonContent>
     </IonPage>
