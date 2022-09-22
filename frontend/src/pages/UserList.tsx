d89d2b79782c6f0fec87c428216113613f0ea3d2
SevenBoii <assy2166@gmail.com>
2022-09-22 10:57:07 +0800

    UserList css update

--- frontend/src/pages/UserList.tsx
+++ frontend/src/pages/UserList.tsx
@@ -1,80 +1,110 @@
-import React, { useEffect, useState } from 'react'
-import { IonPage, IonHeader, IonContent, IonSearchbar, IonCard, IonCardContent, IonImg, IonInfiniteScroll, IonInfiniteScrollContent, IonGrid, IonCol, IonRow, useIonRouter, IonToolbar } from '@ionic/react'
+import React, { useEffect, useState } from "react";
+import {
+  IonPage,
+  IonHeader,
+  IonContent,
+  IonInfiniteScroll,
+  IonInfiniteScrollContent,
+  useIonRouter,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonTitle,
+} from "@ionic/react";
 
-import './css/Team.css'
-import { UserInfo } from '../model'
+import "./css/Common.css";
+import "./css/Team.css";
+import { UserInfo } from "../model";
 
 const UserList: React.FC = () => {
-    const [data, setData] = useState<UserInfo[]>([]);
-    const [isInfiniteDisabled, setInfiniteDisabled] = useState(false);
-    const router = useIonRouter();
+  const [data, setData] = useState<UserInfo[]>([]);
+  const [isInfiniteDisabled, setInfiniteDisabled] = useState(false);
+  const router = useIonRouter();
 
-    useEffect(() => {
-        (async function () {
-            const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/app/user`, {
-                headers: {
-                    'Authorization': `Bearer ${localStorage.getItem('token')}`
-                    }});
-            const result = await res.json();
-            console.log(result)
-            setData(result.user); //remove.user after backend fix
-        })()
-    }, [])
+  useEffect(() => {
+    (async function () {
+      const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/app/user`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      const result = await res.json();
+      console.log(result);
+      setData(result.user); //remove.user after backend fix
+    })();
+  }, []);
 
-    const loadData = (ev: any) => {
-        setTimeout(() => {
-            console.log('Loaded data');
-            ev.target.complete();
-            if (data.length === 100) {
-                setInfiniteDisabled(true);
-            }
-        }, 500);
-    }
+  const loadData = (ev: any) => {
+    setTimeout(() => {
+      console.log("Loaded data");
+      ev.target.complete();
+      if (data.length === 100) {
+        setInfiniteDisabled(true);
+      }
+    }, 500);
+  };
 
-    return (
-        <IonPage>
-            <IonHeader>
-                <IonToolbar>
-                    <IonSearchbar placeholder="Search" onClick={() => { router.push("/search") }} />
-                </IonToolbar>
-            </IonHeader>
-            <IonContent>
-                <IonGrid>
-                    <IonRow>
-                        {data.map((item) => {
-                            return (
-                                <IonCol  key={item.id} size='6'>
-                                    <a href='/teamdetail'>
-                                        <IonCard className="card">
-                                            <IonImg src={`${process.env.REACT_APP_BACKEND_URL}/userUploadedFiles/${item.profilepic}`} style={{ width: '100%' }} />
-                                            <IonCardContent className='content' style={{ fontSize: '10px' }}>
-                                                <p style={{ fontSize: '14px', color: 'white' }}>{item.username}</p><br />
-                                                {item.description}
-                                                <div className='tag'>
-                                                    <span>View</span>
-                                                    <span>View</span>
-                                                </div>
-                                            </IonCardContent>
-                                        </IonCard>
-                                    </a>
-                                </IonCol>
-                            )
-                        })}
-                    </IonRow>
-                    <IonInfiniteScroll
-                        onIonInfinite={loadData}
-                        threshold="100px"
-                        disabled={isInfiniteDisabled}
-                    >
-                        <IonInfiniteScrollContent
-                            loadingSpinner="bubbles"
-                            loadingText="Loading more data..."
-                        ></IonInfiniteScrollContent>
-                    </IonInfiniteScroll>
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonBackButton defaultHref="/tab/home" />
+          </IonButtons>
+          <IonTitle className="title">User List</IonTitle>
+        </IonToolbar>
+        <IonToolbar>
+          <input
+            className="searchbar"
+            placeholder="Search User"
+            onClick={() => {
+              router.push("/search");
+            }}
+          />
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <div className="teamList">
+          {data.map((item) => {
+            return (
+              <div className="teamInfo" key={item.id}>
+                <div
+                  className="teamCard"
+                  onClick={() => {
+                    router.push(`/app/user/${item.id}`);
+                  }}
+                >
+                  <img
+                    className="teamIcon"
+                    src={`${process.env.REACT_APP_BACKEND_URL}/userUploadedFiles/${item.profilepic}`}
+                  />
 
-                </IonGrid>
-            </IonContent>
-        </IonPage>
-    )
-}
-export default UserList
+                  <p className="teamTitle">{item.username}</p>
+
+                  <span className="teamContent">{item.description}</span>
+
+                  <div className="tag">
+                    <span>View</span>
+                    <span>View</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+        <IonInfiniteScroll
+          onIonInfinite={loadData}
+          threshold="100px"
+          disabled={isInfiniteDisabled}
+        >
+          <IonInfiniteScrollContent
+            loadingSpinner="bubbles"
+            loadingText="Loading more data..."
+          ></IonInfiniteScrollContent>
+        </IonInfiniteScroll>
+      </IonContent>
+    </IonPage>
+  );
+};
+export default UserList;
