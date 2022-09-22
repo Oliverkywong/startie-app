7f83bb6745c8e476d242b59853f5bb83b6d070bc
Oliverkywong <tom23400@gmail.com>
2022-09-22 11:36:27 +0800

    serachpage

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -13,168 +13,98 @@ import {
   IonImg,
   IonList,
   IonBackButton,
+  IonTitle,
+  IonCard,
+  IonCardContent,
+  IonCardTitle,
+  IonCol,
 } from "@ionic/react";
-import { flameOutline, trashOutline } from "ionicons/icons";
-import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import cat1 from "../img/cat1.png";
-import cat2 from "../img/cat2.png";
-import cat3 from "../img/cat3.png";
-import "swiper/css";
+import { search } from "ionicons/icons";
+import { useState } from "react";
+import { Team } from "../model";
+// import { flameOutline, trashOutline } from "ionicons/icons";
+// import React from "react";
+// import { Swiper, SwiperSlide } from "swiper/react";
+// import cat1 from "../img/cat1.png";
+// import cat2 from "../img/cat2.png";
+// import cat3 from "../img/cat3.png";
+// import "swiper/css";
 
+  // const catergorys = {
+  //   cat1: { src: cat1, title: "NFT Team" },
+  //   cat2: { src: cat2, title: "Business Team" },
+  //   cat3: { src: cat3, title: "Insevment Team" },
+  // };
 export default function SearchPage() {
+
   const router = useIonRouter();
-  const catergorys = {
-    cat1: { src: cat1, title: "NFT Team" },
-    cat2: { src: cat2, title: "Business Team" },
-    cat3: { src: cat3, title: "Insevment Team" },
-  };
+  const [searchText, setSearchText] = useState("");
+  const [data, setData] = useState<Team[]>([]);
+
+  const searchfetch = async (e:{target: {value: string}}) => {
+    setSearchText(e.target.value)
+    const res = await fetch(
+      `${process.env.REACT_APP_BACKEND_URL}/team/?${searchText}`
+    );
+    const result = await res.json();
+    console.log(result);
+    setData(result);
+    // setData(result.events);
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonSearchbar placeholder="Search" />
+          <IonIcon size="large" icon={search} />
+          <input placeholder="Search" onChange={searchfetch} />
           <IonButtons slot="start">
             <IonBackButton />
           </IonButtons>
-          {/* <IonButtons slot="end">
-                        <IonButton onClick={() => {}}>Cancel</IonButton>
-                    </IonButtons> */}
+          <IonButtons slot="end">
+            <IonButton onClick={() => setSearchText('')}>Cancel</IonButton>
+          </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        <IonToolbar>
-          <IonLabel>Search History</IonLabel>
-          <IonButtons slot="end">
-            <IonButton onClick={() => {}}>
-              <IonIcon icon={trashOutline} />
-            </IonButton>
-          </IonButtons>
-        </IonToolbar>
-        <IonChip style={{ backgroundColor: "#999" }} outline={true}>
-          Outline
-        </IonChip>
-        <IonChip style={{ backgroundColor: "#999" }} outline={true}>
-          Outline
-        </IonChip>
-        <IonChip style={{ backgroundColor: "#999" }} outline={true}>
-          Outline
-        </IonChip>
-        <IonChip style={{ backgroundColor: "#999" }} outline={true}>
-          Outline
-        </IonChip>
-        <IonChip style={{ backgroundColor: "#999" }} outline={true}>
-          Outline
-        </IonChip>
-        <IonToolbar>
-          <IonLabel>Top Search</IonLabel>
-          <IonIcon icon={flameOutline} />
-        </IonToolbar>
-        <IonChip style={{ backgroundColor: "#999" }} outline={true}>
-          Outline
-        </IonChip>
-        <IonChip style={{ backgroundColor: "#999" }} outline={true}>
-          Outline
-        </IonChip>
-        <IonChip style={{ backgroundColor: "#999" }} outline={true}>
-          Outline
-        </IonChip>
-        <IonToolbar>
-          <IonLabel>Team</IonLabel>
-          <IonButtons slot="end">
-            <a href="/tab/team">More</a>
-          </IonButtons>
-        </IonToolbar>
-        <Swiper className="mySwiper" slidesPerView={1}>
-          <SwiperSlide className="catelement">
-            <a href="/tab/team">
-              <IonImg src={catergorys.cat1.src} />
-              <IonLabel>{catergorys.cat1.title}</IonLabel>
-            </a>
-          </SwiperSlide>
-          <SwiperSlide className="catelement">
-            <a href="/tab/team">
-              <IonImg src={catergorys.cat2.src} />
-              <IonLabel>{catergorys.cat2.title}</IonLabel>
-            </a>
-          </SwiperSlide>
-          <SwiperSlide className="catelement">
-            <a href="/tab/team">
-              <IonImg src={catergorys.cat3.src} />
-              <IonLabel>{catergorys.cat3.title}</IonLabel>
-            </a>
-          </SwiperSlide>
-        </Swiper>
-        <IonToolbar>
-          <IonLabel>Event</IonLabel>
-          <IonButtons slot="end">
-            <a href="/tab/event">More</a>
-          </IonButtons>
-        </IonToolbar>
-        <IonList>
-          <a href="/tab/event">
-            <div className="event">
-              <div className="eventinfo">
-                <IonLabel>Event Name</IonLabel>
-                <IonLabel>Desription</IonLabel>
-              </div>
-              <IonImg src={catergorys.cat2.src} style={{ width: "10%" }} />
-            </div>
-          </a>
-          <a href="/tab/event">
-            <div className="event">
-              <div className="eventinfo">
-                <IonLabel>Event Name</IonLabel>
-                <IonLabel>Desription</IonLabel>
-              </div>
-              <IonImg src={catergorys.cat2.src} style={{ width: "10%" }} />
-            </div>
-          </a>
-          <a href="/tab/event">
-            <div className="event">
-              <div className="eventinfo">
-                <IonLabel>Event Name</IonLabel>
-                <IonLabel>Desription</IonLabel>
-              </div>
-              <IonImg src={catergorys.cat2.src} style={{ width: "10%" }} />
-            </div>
-          </a>
-        </IonList>
-        <IonToolbar>
-          <IonLabel>Job</IonLabel>
-          <IonButtons slot="end">
-            <a href="/tab/event">More</a>
-          </IonButtons>
-        </IonToolbar>
-        <IonList>
-          <a href="/tab/event">
-            <div className="event">
-              <div className="eventinfo">
-                <IonLabel>Job Name</IonLabel>
-                <IonLabel>Desription</IonLabel>
-              </div>
-              <IonImg src={catergorys.cat2.src} style={{ width: "10%" }} />
-            </div>
-          </a>
-          <a href="/tab/event">
-            <div className="event">
-              <div className="eventinfo">
-                <IonLabel>Job Name</IonLabel>
-                <IonLabel>Desription</IonLabel>
-              </div>
-              <IonImg src={catergorys.cat2.src} style={{ width: "10%" }} />
-            </div>
-          </a>
-          <a href="/tab/event">
-            <div className="event">
-              <div className="eventinfo">
-                <IonLabel>Job Name</IonLabel>
-                <IonLabel>Desription</IonLabel>
-              </div>
-              <IonImg src={catergorys.cat2.src} style={{ width: "10%" }} />
-            </div>
-          </a>
-        </IonList>
+      <div className="teamList homePageTeamList">
+            {data.map((item) => {
+              return (
+                <IonCol key={item.id}>
+                  <div className="teamInfo">
+                    <IonCard
+                      className="teamCard"
+                      routerLink={`/tab/team/${item.id}`}
+                    >
+                      <img
+                        className="teamIcon"
+                        src={
+                          item?.profilepic != null
+                            ? `${process.env.REACT_APP_BACKEND_URL}/userUploadedFiles/${item.profilepic}`
+                            : "https://www.w3schools.com/howto/img_avatar.png"
+                        }
+                      />
+
+                      <IonCardTitle className="teamTitle">
+                        {item.name}
+                      </IonCardTitle>
+
+                      <IonCardContent className="teamContent">
+                        {item.description}
+                      </IonCardContent>
+
+                      <div className="tag">
+                        {item.tags.map((tag) => {
+                          return <span key={tag}>{tag}</span>;
+                        })}
+                      </div>
+                    </IonCard>
+                  </div>
+                </IonCol>
+              );
+            })}
+          </div>
+       
       </IonContent>
     </IonPage>
   );
