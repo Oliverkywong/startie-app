0e232c4787cbf09e067ce89abe3808c62a85cd00
Oliverkywong <tom23400@gmail.com>
2022-09-23 11:01:20 +0800

    fix user list bug

--- frontend/src/pages/Recommend.tsx
+++ frontend/src/pages/Recommend.tsx
@@ -1,78 +1,63 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 import { IonButton, IonCard, IonCardContent, IonContent, IonHeader, IonImg, IonLabel, IonList, IonPage, useIonRouter } from '@ionic/react'
 import icon from '../img/tonystarkicon.png'
+import { UserInfo } from '../model';
 
 export default function Recommend() {
+    const [data, setData] = useState<UserInfo[]>([]);
+    const [isInfiniteDisabled, setInfiniteDisabled] = useState(false);
     const router = useIonRouter();
+
+    useLayoutEffect(() => {
+        (async function () {
+            const res = await fetch(`${process.env.REACT_APP_BACKEND_URL}/app/user`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+            });
+            const result = await res.json();
+            console.log(result);
+            setData(result.user.slice(0,4)); //remove.user after backend fix
+        })();
+    }, []);
+
     return (
         <IonPage>
             <IonHeader>
                 <IonHeader>AI Suggestion</IonHeader>
             </IonHeader>
             <IonContent>
-                <IonList>
-                    <a href='#'>
-                        <IonCard>
-                            <div className="event">
-                                <IonImg src={icon} style={{ width: '10%' }} />
-                                <div className="eventinfo">
-                                    <IonLabel>Name</IonLabel>
-                                    <IonLabel>Date</IonLabel>
+                <div className="teamList">
+                    {data.map((item) => {
+                        return (
+                            <div className="teamInfo" key={item.id}>
+                                <div
+                                    className="teamCard"
+                                    onClick={() => {
+                                        router.push(`/tab/user/${item.id}`);
+                                    }}
+                                >
+                                    <img
+                                        className="teamIcon"
+                                        src={`${process.env.REACT_APP_BACKEND_URL}/userUploadedFiles/${item.profilepic}`}
+                                    />
+
+                                    <p className="teamTitle">{item.username}</p>
+
+                                    <p className="teamContent">{item.shortDescription}</p>
+
+                                    <div className="tag">
+                                        {item.tags.map((tag) => {
+                                            return <span key={tag}>{tag}</span>;
+                                        })}
+                                    </div>
                                 </div>
-                                <IonButton>INVITE</IonButton>
                             </div>
-                            <IonCardContent>
-                                Descrption
-                            </IonCardContent>
-                        </IonCard>
-                    </a>
-                    <a href='#'>
-                        <IonCard>
-                            <div className="event">
-                                <IonImg src={icon} style={{ width: '10%' }} />
-                                <div className="eventinfo">
-                                    <IonLabel>Name</IonLabel>
-                                    <IonLabel>Date</IonLabel>
-                                </div>
-                                <IonButton>INVITE</IonButton>
-                            </div>
-                            <IonCardContent>
-                                Descrption
-                            </IonCardContent>
-                        </IonCard>
-                    </a>
-                    <a href='#'>
-                        <IonCard>
-                            <div className="event">
-                                <IonImg src={icon} style={{ width: '10%' }} />
-                                <div className="eventinfo">
-                                    <IonLabel>Name</IonLabel>
-                                    <IonLabel>Date</IonLabel>
-                                </div>
-                                <IonButton>INVITE</IonButton>
-                            </div>
-                            <IonCardContent>
-                                Descrption
-                            </IonCardContent>
-                        </IonCard>
-                    </a>
-                    <a href='#'>
-                        <IonCard>
-                            <div className="event">
-                                <IonImg src={icon} style={{ width: '10%' }} />
-                                <div className="eventinfo">
-                                    <IonLabel>Name</IonLabel>
-                                    <IonLabel>Date</IonLabel>
-                                </div>
-                                <IonButton>INVITE</IonButton>
-                            </div>
-                            <IonCardContent>
-                                Descrption
-                            </IonCardContent>
-                        </IonCard>
-                    </a>
-                </IonList>
-                <IonLabel onClick={()=>router.push('tab/home')}>No Thanks</IonLabel>
+                        );
+                    })}
+                </div>
+
+                <IonLabel onClick={() => router.push('tab/home')}>No Thanks</IonLabel>
             </IonContent>
         </IonPage>
     )
