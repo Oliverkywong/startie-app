97ab6c8fe7dfb58f68634d81a99e6c7ca9e41308
SevenBoii <assy2166@gmail.com>
2022-09-23 16:20:10 +0800

    merge

--- backend/routes/sectorskillRoute.ts
+++ backend/routes/sectorskillRoute.ts
@@ -1,13 +1,12 @@
 import express from "express";
 import { SectorskillController } from "../controllers/sectorskillController";
-import { isLogin } from "../utils/middleware";
 
+export function sectorskillRoutes(
+  sectorskillController: SectorskillController
+) {
+  const sectorskillRoute = express.Router();
 
+  sectorskillRoute.get("/skill", sectorskillController.allSectorskill);
 
-export function sectorskillRoutes(sectorskillController:SectorskillController) {
-    const sectorskillRoute = express.Router();
-
-    sectorskillRoute.get('/skill', isLogin, sectorskillController.allSectorskill)
-
-    return sectorskillRoute;
+  return sectorskillRoute;
 }
