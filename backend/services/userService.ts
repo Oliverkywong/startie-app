a579860d1a8ef09877692b05a568ff40647a3cc8
SevenBoii <assy2166@gmail.com>
2022-09-23 10:46:15 +0800

    userList display error fixed

--- backend/services/userService.ts
+++ backend/services/userService.ts
@@ -107,8 +107,10 @@ export class UserService {
     }
 
     if (
-      !(username === userRecord[0].username &&
-        (await checkPassword(password, userRecord[0].password)))
+      !(
+        username === userRecord[0].username &&
+        (await checkPassword(password, userRecord[0].password))
+      )
     ) {
       throw new UserPasswordMissMatchError();
     }
@@ -128,16 +130,31 @@ export class UserService {
     return userRecord;
   }
   // -------------------------------------------------------------------------------------------------------------------
-  // get all User Info
+  // get all User Info (Admin & search)
   // -------------------------------------------------------------------------------------------------------------------
-  async getAllUser(input:UserListInput, show?: boolean): Promise<UserListData>  {
-
+  async getAllUser(
+    input: UserListInput,
+    show?: boolean
+  ): Promise<UserListData> {
     let query = this.knex<User>("user")
-    .select("user.id", "username", "email", "phonenumber","isadmin", "s.name as status", this.knex.raw('ARRAY_AGG(distinct t.name) as tags'), "description", "shortDescription", "profilepic", "clickrate", "created_at")
-    .join("status as s", "status_id", "s.id")
-    .leftJoin("user_tag", "user.id", "user_tag.user_id")
-    .join("tag as t", "user_tag.tag_id", "t.id")
-    .groupBy("user.id", "s.id")
+      .select(
+        "user.id",
+        "username",
+        "email",
+        "phonenumber",
+        "isadmin",
+        "s.name as status",
+        this.knex.raw("ARRAY_AGG(distinct t.name) as tags"),
+        "description",
+        "shortDescription",
+        "profilepic",
+        "clickrate",
+        "created_at"
+      )
+      .join("status as s", "status_id", "s.id")
+      .leftJoin("user_tag", "user.id", "user_tag.user_id")
+      .join("tag as t", "user_tag.tag_id", "t.id")
+      .groupBy("user.id", "s.id");
 
     if (input.name) {
       query = query.having("username", "ilike", `%${input.name}%`);
@@ -155,46 +172,76 @@ export class UserService {
       query = query.having("description", "ilike", `%${input.description}%`);
     }
     if (input.shortDescription) {
-      query = query.having("shortDescription", "ilike", `%${input.shortDescription}%`);
+      query = query.having(
+        "shortDescription",
+        "ilike",
+        `%${input.shortDescription}%`
+      );
     }
     if (input.phonenumber) {
       query = query.having("phonenumber", "ilike", `%${input.phonenumber}%`);
     }
     if (input.tags) {
-      query = query.having(this.knex.raw(`array_agg(distinct t.name)::VARCHAR ilike '%${input.tags}%'`));
+      query = query.having(
+        this.knex.raw(
+          `array_agg(distinct t.name)::VARCHAR ilike '%${input.tags}%'`
+        )
+      );
     }
     if (show) {
-      query = query.orderBy('id', 'asc')
+      query = query.orderBy("id", "asc");
     } else {
-      query = query.orderBy('id', 'asc').where('status_id', 1)
+      query = query.orderBy("id", "asc").where("status_id", 1);
     }
     let user = await query;
 
-    return {user};
+    return { user };
+  }
+  // -------------------------------------------------------------------------------------------------------------------
+  // get all User List
+  // -------------------------------------------------------------------------------------------------------------------
+  async getAllUserList() {
+    const userRecord = await this.knex<User>("user")
+      .select(
+        "user.id",
+        "username",
+        this.knex.raw("ARRAY_AGG(distinct t.name) as tags"),
+        "shortDescription",
+        "profilepic",
+        "clickrate"
+      )
+      .join("status as s", "status_id", "s.id")
+      .leftJoin("user_tag", "user.id", "user_tag.user_id")
+      .join("tag as t", "user_tag.tag_id", "t.id")
+      .groupBy("user.id", "s.id");
+    return userRecord;
   }
   // -------------------------------------------------------------------------------------------------------------------
   // edit User Info (Admin)
   // -------------------------------------------------------------------------------------------------------------------
   async editUserForAdmin(userId: number, input: UserListInput) {
-    
     const userRecord = await this.knex<User>("user")
       .update({
         profilepic: input.profilepic,
         isadmin: input.isadmin,
         phonenumber: input.phonenumber,
         description: input.description,
-        status_id: input.status_id
+        status_id: input.status_id,
       })
       .where("id", userId)
       .returning("*");
 
-    return userRecord
+    return userRecord;
   }
-// -------------------------------------------------------------------------------------------------------------------
-// edit User Info
-// -------------------------------------------------------------------------------------------------------------------
-  async editUser(userId: number, profilepic: string, phonenumber: number | string, description: string) {
-    
+  // -------------------------------------------------------------------------------------------------------------------
+  // edit User Info
+  // -------------------------------------------------------------------------------------------------------------------
+  async editUser(
+    userId: number,
+    profilepic: string,
+    phonenumber: number | string,
+    description: string
+  ) {
     const userRecord = await this.knex<User>("user")
       .update({
         profilepic: profilepic,
@@ -204,7 +251,7 @@ export class UserService {
       .where("id", userId)
       .returning("*");
 
-    return userRecord
+    return userRecord;
   }
 
   // -------------------------------------------------------------------------------------------------------------------
