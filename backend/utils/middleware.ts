6b8c3ecab56b895e6b6b7dc1604bd0679da395dc
HY Chan <hychan@HYdeMacBook-Air.local>
2022-09-23 16:32:58 +0800

    backend response msg update

--- backend/utils/middleware.ts
+++ backend/utils/middleware.ts
@@ -44,11 +44,13 @@ export const isLogin = async (
     }
   } catch (e) {
     if (e.code === "ERR_JWT_EXPIRED") {
+      res.status(401).json({ result: false, msg: "Login Expired" });
+    } else if (e.code === 'Invalid Compact JWS'){
       res.status(401).json({ result: false, msg: "You have to login first!" });
     } else {
       res.status(500).json({ result: false, msg: "Incorrect Token" });
     }
-  }
+    }
 };
 
 // -------------------------------------------------------------------------------------------------------------------
