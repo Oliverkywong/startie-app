44f1e2993d9c500201f3d07fe70b475bec5cef1d
SevenBoii <assy2166@gmail.com>
2022-09-26 04:43:09 +0800

    user belongs team bug fix half done

--- frontend/src/pages/component/UserTeams.tsx
+++ frontend/src/pages/component/UserTeams.tsx
@@ -4,37 +4,36 @@ import { Team } from "../../model";
 import { API_ORIGIN } from "../../utils/api";
 
 import "../css/Common.css";
-import "../css/UserTeam.css"; 
-
+import "../css/UserTeam.css";
 
 export default function UserTeams(props: { team: Team[] }) {
   const router = useIonRouter();
   return (
-      <div className="ProfileBackground">
-        {props.team.map((team) => {
-          return (
-            <div
-              className="userTeam"
-              key={team.id}
-              onClick={() => router.push(`team/${team.id}`)}
-            >
-              <IonImg
-                src={
-                  team?.profilepic != null
-                    ? `${API_ORIGIN}/userUploadedFiles/${team.profilepic}`
-                    : "https://www.w3schools.com/howto/img_avatar.png"
-                }
-              ></IonImg>
-              <div>
-                <IonTitle>{team.name}</IonTitle>
-              </div>
-
-              <div>
-                <IonLabel>{team.description}</IonLabel>
-              </div>
+    <div>
+      {props.team.map((team) => {
+        return (
+          <div
+            className="userTeam"
+            key={team.id}
+            onClick={() => router.push(`team/${team.id}`)}
+          >
+            <IonImg
+              src={
+                team?.profilepic != null
+                  ? `${API_ORIGIN}/userUploadedFiles/${team.profilepic}`
+                  : "https://www.w3schools.com/howto/img_avatar.png"
+              }
+            />
+            <div>
+              <IonTitle>{team.name}</IonTitle>
             </div>
-          );
-        })}
-      </div>
+
+            <div>
+              <IonLabel>{team.description}</IonLabel>
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 }
