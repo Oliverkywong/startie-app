12bd3210a5064340743a290687a73a048a11df38
HY Chan <hychan@HYdeMacBook-Air.local>
2022-09-27 16:02:18 +0800

    search page team list bug fixed

--- frontend/src/pages/EventList.tsx
+++ frontend/src/pages/EventList.tsx
@@ -107,7 +107,7 @@ const EventList: React.FC = () => {
                   <div>
                     <p className="eventDescription">{item.provider_name}</p>
                     <p className="eventDate">
-                      Due: {moment().format(item.starttime)}
+                      Due: {moment(item.starttime).format("DD/MM/YYYY")}
                     </p>
                   </div>
                 </div>
